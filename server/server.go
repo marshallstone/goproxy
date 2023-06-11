d0d6990b4a18fe3086b6f8f8535272252b131e0d
marshall <stmr@umich.edu>
2023-06-11 12:25:03 -0400

    server: get initial packet from client

--- server/server.go
+++ server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
@@ -32,14 +34,42 @@ func main() {
 }
 
 func handleRequest(conn net.Conn) {
-	var msg lib.InitPacket
+	// Use the same buffer for read/write operations?
 	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	if n == 3 {
-		fmt.Printf("version: %d\n# auth methods supported: %d\nauth method: %d\n", buf[0], buf[1], buf[2])
+	for {
+		n, err := conn.Read(buf)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Initial client message, start handhsake
+		if n == 3 {
+			initMsg := lib.ServerMethod{Version: buf[0], Auth: buf[1], Method: buf[2]}
+
+			if initMsg.Version != 5 {
+				log.Fatal("socks version < 5 not supported.\n")
+			}
+
+			// Send method with no auth (no subnegotiation required)
+			resp := lib.ClientMethod{Version: 0x5, Method: 0x0}
+			respBuf := new(bytes.Buffer)
+			err = binary.Write(respBuf, binary.BigEndian, resp)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			conn.Write(respBuf.Bytes())
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// Handle requests next
+			// Form TCP connection to forward requests
+			fmt.Printf("%d bytes read\n", n)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
